34e0fe995a4f0902398e91a0cf33fb8a18055684
website-krysta <website.krysta@gmail.com>
2025-09-17 14:10:39 +0530

    change vite js code

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    // base: "/",
+  base: mode === "production" ? "/dharma-foods-usa/" : "/",
   server: {
     host: "::",
     port: 8080,
